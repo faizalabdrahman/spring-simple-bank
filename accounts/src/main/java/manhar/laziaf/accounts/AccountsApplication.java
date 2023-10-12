29de386fc6a8934757248afbcaba6aa37d4bbf77
faizalabdrahman <faizalabdrahman@gmail.com>
2023-10-12 13:36:46 +0800

    Added openapi. Closed #6.

--- accounts/src/main/java/manhar/laziaf/accounts/AccountsApplication.java
+++ accounts/src/main/java/manhar/laziaf/accounts/AccountsApplication.java
@@ -1,9 +1,24 @@
 package manhar.laziaf.accounts;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Simple Bank Accounts Microservice REST API Documentation",
+				description = "Simple Bank Accounts Microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Muhammad Faizal",
+						email = "faizalabdrahman@gmail.com",
+						url = "https://www.linkedin.com/in/faizalabdrahman/"
+				)
+		)
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
--- accounts/src/main/java/manhar/laziaf/accounts/web/controllers/AccountsController.java
+++ accounts/src/main/java/manhar/laziaf/accounts/web/controllers/AccountsController.java
@@ -1,5 +1,9 @@
 package manhar.laziaf.accounts.web.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import manhar.laziaf.accounts.constants.AccountsConstants;
@@ -13,6 +17,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(
+        name = "CRUD REST APIs for Accounts in Simple Bank",
+        description = "CRUD REST APIs in Simple Bank to CREATE, UPDATE, FETCH, DELETE account details"
+)
 @RestController
 @RequestMapping(path = "/api/v1/accounts", produces = {MediaType.APPLICATION_JSON_VALUE})
 @Validated
@@ -24,11 +32,27 @@ public class AccountsController {
         this.accountService = accountService;
     }
 
+    @Operation(
+            summary = "Create Account REST API",
+            description = "REST API to create new customer and account in Simple Bank"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description =  "HTTP status created"
+    )
     @PostMapping("/create")
     public ResponseEntity<AccountDto> createAccount(@RequestBody @Valid CustomerDto customerDto) {
         return new ResponseEntity<>(accountService.createAccount(customerDto), HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "Get Account Details REST API",
+            description = "REST API to get customer and account details based on mobile number"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description =  "HTTP status ok"
+    )
     @GetMapping("/get")
     public ResponseEntity<CustomerDto> getAccountDetails(@RequestParam
                                                              @Pattern(regexp = "(^$|[0-9]{8})", message = "Mobile Number must be numeric")
@@ -36,6 +60,20 @@ public class AccountsController {
         return new ResponseEntity<>(accountService.getAccount(mobileNumber), HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Update Account REST API",
+            description = "REST API to update customer and account based on account number"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description =  "HTTP status ok"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description =  "HTTP status internal server error"
+            )
+    })
     @PutMapping("/update")
     public ResponseEntity<ResponseDto> updateAccountDetails(@RequestBody @Valid CustomerDto customerDto) {
         CustomerDto updatedCustomerDto = accountService.updateAccount(customerDto);
@@ -45,6 +83,20 @@ public class AccountsController {
         return new ResponseEntity<>(new ResponseDto(AccountsConstants.MESSAGE_500), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    @Operation(
+            summary = "Delete Account REST API",
+            description = "REST API to delete customer and account base on mobile number"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description =  "HTTP status ok"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description =  "HTTP status internal server error"
+            )
+    })
     @DeleteMapping("/delete")
     public ResponseEntity<ResponseDto> deleteAccountDetails(@RequestParam
                                                                 @Pattern(regexp = "(^$|[0-9]{8})", message = "Mobile Number must be numeric")
